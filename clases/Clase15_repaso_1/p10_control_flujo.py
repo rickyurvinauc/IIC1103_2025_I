b71610695e2db97a2b412932803c69c730c4826a
ricardourvina <rurvinac@estudiante.uc.cl>
2025-04-22 21:44:09 -0400

    Clase 15

--- clases/Clase15_repaso_1/p10_control_flujo.py
+++ clases/Clase15_repaso_1/p10_control_flujo.py
@@ -1,19 +1,15 @@
-# Escribe tu código aquí
-
 cantidad_tickets = 0
-cantidad_maxima = 100
 cantidad_entran = 0
 while True:
     ticket = input()
-    ultimos_dos = ticket[-2] + ticket[-1]
+    cantidad_a_entrar = int(ticket[-2:])
     cantidad_tickets += 1
-    cantidad_a_entrar = int(ultimos_dos)
     cantidad_entran += cantidad_a_entrar
     cantidad_permitida = 100 - cantidad_entran
     if cantidad_permitida < 0:
         quedaron_fuera = cantidad_permitida * -1
         entraron = cantidad_a_entrar - quedaron_fuera
-        print(f"{entraron} entraron y {cantidad_permitida * -1} quedaron fuera con el ticket {ticket}")
+        print(f"{entraron} entraron y {quedaron_fuera} quedaron fuera con el ticket {ticket}")
         if cantidad_tickets >= 10:
             print("No leo mas tickets y se lleno.")
             break
@@ -24,9 +20,3 @@ while True:
         if cantidad_tickets == 10:
             print("No leo mas tickets.")
             break
-
-    if cantidad_tickets >= 10:
-        print("No leo mas tickets y se lleno.")
-        break
-
-
